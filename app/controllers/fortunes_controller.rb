14108a790409a1200fa1ec581ba5e0c7c6282218
Raghu Betina <rbetina@gmail.com>
2013-10-21 12:41:08 -0500

    Using .each in the view template

--- app/controllers/fortunes_controller.rb
+++ app/controllers/fortunes_controller.rb
@@ -1,10 +1,6 @@
 class FortunesController < ApplicationController
   def numbers
-    @lucky_number_1 = rand(1..100)
-    @lucky_number_2 = rand(1..100)
-    @lucky_number_3 = rand(1..100)
-    @lucky_number_4 = rand(1..100)
-    @lucky_number_5 = rand(1..100)
+    @lucky_numbers = [rand(1..100), rand(1..100), rand(1..100), rand(1..100), rand(1..100)]
 
     render("numbers")
   end
